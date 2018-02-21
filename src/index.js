611ea2d184be08a1fc54ed47e3d4959c5743b73b
Denis <den107620@gmail.com>
2018-02-21 23:21:03 +0300

    quadratic-equation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  var arr = equation.split(' '),
+      o = [];
+    a = parseInt(arr[0]);
+    b = parseInt(arr[3] + arr[4]);
+    c = parseInt(arr[7] + arr[8]);
+
+    desk = b*b - 4*a*c;
+
+    if (desk > 0){
+      x1 = (((-b) + Math.sqrt(desk))/(2 * a));
+      x2 = (((-b) - Math.sqrt(desk))/(2 * a));
+    } else if(desk = 0){
+      x1 = -(b/(2*a));
+      x2 = x1;
+    }
+    x1 = Math.round(x1);
+    o.push(x1);
+    x2 = Math.round(x2);
+    o.push(x2);
+
+    function Numeric(a, b) {
+      if (a > b) return 1;
+      if (a < b) return -1;
+    };
+    o.sort(Numeric);
+    return o;
 }
